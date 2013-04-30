16aeaf9e14a2de8025ae470ccf21f1b4f2134088
Yasushi Kumano <kumano.0x96f5@gmail.com>
2013-04-30 21:50:15 +0900

    commit object files.

--- code/objs/src/LedDriver/LedDriver.d
+++
@@ -1,7 +0,0 @@
-objs/./src/LedDriver/LedDriver.o: src/LedDriver/LedDriver.c \
-  unity.framework/extras/fixture/src/unity_fixture_malloc_overrides.h \
-  include/LedDriver/LedDriver.h
-
-unity.framework/extras/fixture/src/unity_fixture_malloc_overrides.h:
-
-include/LedDriver/LedDriver.h:
